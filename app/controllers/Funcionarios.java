130798740837c3d9b81abcdce19455cdee9ea4d9
Pablo Souza <pvrsouza@gmail.com>
2012-04-09 16:13:23 -0300

    limpeza de metodos nao mais utilizados

--- app/controllers/Funcionarios.java
+++ app/controllers/Funcionarios.java
@@ -10,21 +10,11 @@ import play.modules.paginate.ValuePaginator;
 import play.mvc.Controller;
 
 public class Funcionarios extends Controller{
+	
 	public static void index(){
 		List<Funcionario> listaFuncionarios = Funcionario.findAll();
 		ValuePaginator paginator = new ValuePaginator(listaFuncionarios);
-		paginator.setPageSize(10);
+		paginator.setPageSize(15);
 	    render(paginator);
-	}
-	
-	public static void simular(){
-		Funcionario fun = (Funcionario) Funcionario.findById(new Long(1));
-		flash.success("O registro " + fun.getCpfFormatado() + " apagado com sucesso!");
-		index();
-	}
-	
-	public static void demo(){
-		List<Funcionario> listaFuncionarios = Funcionario.findAll();
-		render(listaFuncionarios);
 	}
 }
