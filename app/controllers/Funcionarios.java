3e0d24584e89d4469d661c3eb53b8f8c9a812d6b
Pablo Souza <pvrsouza@gmail.com>
2012-04-09 08:28:07 -0300

    conclusao da classe Funcionarios conforme reuniao com elquisson dia 08/04/2010

--- app/controllers/Funcionarios.java
+++ app/controllers/Funcionarios.java
@@ -12,4 +12,15 @@ public class Funcionarios extends Controller{
 		List<Funcionario> listaFuncionarios = Funcionario.findAll();
 		render(listaFuncionarios);		
 	}
+	
+	public static void simular(){
+		Funcionario fun = (Funcionario) Funcionario.findById(new Long(1));
+		flash.success("O registro " + fun.getCpfFormatado() + " apagado com sucesso!");
+		index();
+	}
+	
+	public static void demo(){
+		List<Funcionario> listaFuncionarios = Funcionario.findAll();
+		render(listaFuncionarios);
+	}
 }
